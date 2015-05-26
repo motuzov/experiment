d3cef8eba72de0bb202b5af75e6627a7f2df0e21
motuzov <motuzov@gmail.com>
2015-05-26 08:45:35 +0300

    simple crawler

--- scraping/selenium_tools.py
+++ scraping/selenium_tools.py
@@ -1,11 +1,20 @@
-import time
 from selenium import webdriver
- 
+import json
 if __name__ == '__main__':
     driver = webdriver.Firefox()
     driver.get("http://salonemilano.it/en-us/EXHIBITORS/Exhibitor-List-2015")
-    elem = driver.find_element_by_id("dnn_ctr4578_View_repLettere_lnklettere_26")
-    print elem.text
-    elem.click()
-    for e in driver.find_elements_by_class_name("inserisci"):
-        print e.text
+    abclist = driver.find_element_by_id("dnn_ctr4578_View_divAbc")
+    res = []
+    refIds = [ref.get_attribute("id") for ref in abclist.find_elements_by_tag_name("a")]
+    for refId in refIds:
+        print refId
+        elem = driver.find_element_by_id(refId)
+        print elem.text
+        elem.click()
+        for e in driver.find_elements_by_class_name("livinchart"):
+            compattr = e.text.split('\n')
+            print compattr[6]
+            res.append(compattr)
+    with open("salonemilano.txt", "w") as fout:
+        fout.write(json.dumps(res))
+
